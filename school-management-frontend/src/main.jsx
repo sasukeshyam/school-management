01abf67ee424c8ecab822bc4023f2bdae56208d6
sasukeshyam <akashpandit790@gmail.com>
2026-06-17 16:59:57 +0530

    for test in main

--- school-management-frontend/src/main.jsx
+++ school-management-frontend/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 import './index.css'
+// for test
+console.log('DEPLOY TEST 12345')
 
 const queryClient = new QueryClient({
   defaultOptions: {
