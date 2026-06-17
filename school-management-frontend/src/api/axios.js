4cc20a53aee6835217c8bc6b4e667318fc0eb59d
sasukeshyam <akashpandit790@gmail.com>
2026-06-17 12:34:17 +0530

    axios.js baseurl

--- school-management-frontend/src/api/axios.js
+++ school-management-frontend/src/api/axios.js
@@ -1,20 +1,21 @@
 import axios from 'axios'
 import { useAuthStore } from '@/store/authStore'
 
-console.log('VITE_API_URL:', import.meta.env.VITE_API_URL);
-console.log(
-  'BASE URL:',
-  import.meta.env.VITE_API_URL
-    ? `${import.meta.env.VITE_API_URL}/api/v1`
-    : '/api/v1'
-);
 
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL
+//     ? `${import.meta.env.VITE_API_URL}/api/v1`
+//     : '/api/v1',
+//   withCredentials: true,
+//   headers:        { 'Content-Type': 'application/json' },
+// })
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
-    ? `${import.meta.env.VITE_API_URL}/api/v1`
-    : '/api/v1',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
-  headers:        { 'Content-Type': 'application/json' },
+  headers: {
+    'Content-Type': 'application/json',
+  },
 })
 
 // Attach access token
